c405c1f5d0e2a08459d4c1240dbf4a51f9d7fd25
Ludwig Bedacht <ludwig.bedacht@gmail.com>
2023-08-22 10:16:48 +0200

    comitting changes

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -1,9 +1,15 @@
 package controller
 
+import "github.com/eurofurence/reg-room-service/internal/database"
+
 // Controller is the service interface, which defines
 // the functions in the service layer of this application
 //
 // A type implementing this interface provides functionality
 // to interact between the web layer and the data layer
 type Controller interface {
+}
+
+type serviceController struct {
+	DB database.Repository
 }
